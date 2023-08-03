7277739114dbae05cc79554dd22e8c6d4ce56496
Carlos3wd <133605817+Carlos3wd@users.noreply.github.com>
2023-08-03 17:46:22 -0300

    js
    adicionando variables

--- Clone-flappy/game.js
+++ Clone-flappy/game.js
@@ -12,4 +12,37 @@ const FLAP_SPEED = -5;
 const BIRD_WIDTH = 40;
 const BIRD_HEIGTH = 30;
 const PIPE_WIDTH = 50;
-const PIPE_GAP = 125;
+const PIPE_GAP = 125; 
+
+// Bird variables
+let birdX = 50;
+let birdY = 50;
+let birdVelocity = 0;
+let birdAcceleration = 0.1;
+
+// Pipe variables
+let pipeX = 400;
+let papiY = canvas.heigth - 200;
+
+// score 
+let scoreDiv = document.getElementById('score-display')
+let score = 0;
+let highScore = 0;
+
+function increaseScore() {
+     // TODO:
+}
+
+function collisionCheck() {
+     // TODO:
+}
+function resetGame() {
+     // TODO:
+}
+
+function endGame() {
+    // TODO:
+}
+function loop() {
+    // TODO:
+}
